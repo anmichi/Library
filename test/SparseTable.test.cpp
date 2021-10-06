f3d4a42fb86071f70bb2217b5a58c8f717dcae81
anmichi <anmichi.anmichi@gmail.com>
2021-10-07 00:10:32 +0900

    add test file

---
+++ test/SparseTable.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include "../SparseTable.cpp"
+using namespace std;
+int op(int a, int b) { return min(a, b); }
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+    SparseTable<int, op> st(a);
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << st.query(l, r) << endl;
+    }
+}
