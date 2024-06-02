e217d6dbe265764da9054824f4300b0392bb49cf
anmichi <47889950+anmichi@users.noreply.github.com>
2024-06-02 17:26:00 +0900

    dst

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -26,3 +26,35 @@ struct sparsetable {
         return op(table[len][l], table[len][r - (1 << len)]);
     }
 };
+template <class T, T (*op)(T, T), T (*e)()>
+struct disjointsparsetable {
+    vector<vector<T>> table;
+    vector<int> logtable;
+    disjointsparsetable(vector<T> v) {
+        int len = 0;
+        while ((1 << len) <= v.size()) len++;
+        table.assign(len, vector<T>(1 << len, e()));
+        for (int i = 0; i < (int)v.size(); i++) table[0][i] = v[i];
+        for (int i = 1; i < len; i++) {
+            int shift = 1 << i;
+            for (int j = 0; j < (int)v.size(); j += shift << 1) {
+                int t = min(j + shift, (int)v.size());
+                table[i][t - 1] = v[t - 1];
+                for (int k = t - 2; k >= j; k--) table[i][k] = op(v[k], table[i][k + 1]);
+                if (v.size() <= t) break;
+                table[i][t] = v[t];
+                int r = min(t + shift, (int)v.size());
+                for (int k = t + 1; k < r; k++) table[i][k] = op(table[i][k - 1], v[k]);
+            }
+        }
+        logtable.resize(1 << len);
+        for (int i = 2; i < logtable.size(); i++) {
+            logtable[i] = logtable[(i >> 1)] + 1;
+        }
+    }
+    T query(int l, int r) {
+        if (l >= --r) return table[0][l];
+        int len = logtable[l ^ r];
+        return op(table[len][l], table[len][r]);
+    };
+};
