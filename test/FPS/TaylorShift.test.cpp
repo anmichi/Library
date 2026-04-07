c3e8e811792303fe219077dc8bd9c3c8c16d1695
anmichi <anmichi@LAPTOP-3I14I4EE.localdomain>
2026-04-07 16:32:06 +0900

    fix

--- test/FPS/TaylorShift.test.cpp
+++ test/FPS/TaylorShift.test.cpp
@@ -1,6 +1,6 @@
 // competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/polynomial_taylor_shift
 #include "../../FPS/TaylorShift.cpp"
-#include "../modint_acl_io.cpp"
+#include "../../modint_acl_io.cpp"
 int main() {
     using mint = atcoder::modint998244353;
     int n;
@@ -11,6 +11,6 @@ int main() {
     for (auto& x : f) cin >> x;
     Binomial<mint> bin(n);
     FormalPowerSeries<mint> g = TaylorShift<mint>(f, c, bin);
-    rep(i, n) cout << g[i] << " ";
+    for (int i = 0; i < n; i++) cout << g[i] << " ";
     cout << endl;
 }
